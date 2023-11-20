a175dd1bc54d27b83fc91aa592edfd06f36c5ba7
Williams Calzadilla <132520567+Lowganma@users.noreply.github.com>
2023-11-20 12:56:22 -0400

    main.py

--- main.py
+++ main.py
@@ -1,35 +1,39 @@
 from funciones import Modificar_Producto,AgregarProducto,Buscar_producto, Mostrar_Producto, Exportar_Excel, Borrar_Producto
 
 print("Programa de gestion de inventarios".center(60,"-"))
+def main():
 
-while True:
-    try:
-        print("""
-        (1) Anadir productos
-        (2) Buscar productos
-        (3) Modificar productos
-        (4) Ver productos
-        (5) borrar producto 
-        (6) guardar
-        (7) salir      
-        """)
+    while True:
+        try:
+            print("""
+            (1) Anadir productos
+            (2) Buscar productos
+            (3) Modificar productos
+            (4) Ver productos
+            (5) borrar producto 
+            (6) guardar
+            (7) salir      
+            """)
 
-        respuesta = int(input("Ingrese su opcion: "))
-        if respuesta == 1:
-            AgregarProducto()
-        elif respuesta == 2:
-            print(Buscar_producto())
-        elif respuesta == 3:
-            Modificar_Producto()
-        elif respuesta == 4:
-            print(Mostrar_Producto())
-        elif respuesta == 5:
-            Borrar_Producto()
-        elif respuesta == 6:
-            Exportar_Excel()
-        else:
-            break
-    except ValueError:
-        print("Por favor, ingrese un número válido.")        
-    except Exception as e:
-        print("Ocurrió un error:", e)    
+            respuesta = int(input("Ingrese su opcion: "))
+            if respuesta == 1:
+                AgregarProducto()
+            elif respuesta == 2:
+                print(Buscar_producto())
+            elif respuesta == 3:
+                Modificar_Producto()
+            elif respuesta == 4:
+                print(Mostrar_Producto())
+            elif respuesta == 5:
+                Borrar_Producto()
+            elif respuesta == 6:
+                Exportar_Excel()
+            else:
+                break
+        except ValueError:
+            print("Por favor, ingrese un número válido.")        
+        except Exception as e:
+            print("Ocurrió un error:", e)    
+
+if __name__ == "__main__":
+    main()
