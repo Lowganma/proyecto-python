ab232daf0e32b1b10e94d08f09da82cef0ff96ab
Williams Calzadilla <132520567+Lowganma@users.noreply.github.com>
2023-11-26 22:37:40 -0400

    Update funciones.py

--- funciones.py
+++ funciones.py
@@ -3,27 +3,17 @@ cantidad = []
 productos = []
 precio = [] 
 
-def AgregarProducto():
-    añadir_cantidad = int(input("Ingrese la cantidad de su producto: "))
-    añadir_producto = input("Ingrese el nombre de su producto: ")
-    añadir_precio = float(input("Ingrese el precio de su producto: "))
-                
+def AgregarProducto(añadir_cantidad,añadir_producto,añadir_precio):         
     cantidad.append(añadir_cantidad)
     productos.append(añadir_producto)
     precio.append(añadir_precio)
 
-def Buscar_producto(): 
-    buscador = input("Ingrese el nombre del producto que quiere buscar: ")
+def Buscar_producto(buscador): 
     posicion = productos.index(buscador)
     return (cantidad[posicion],productos[posicion], precio[posicion])
 
-def Modificar_Producto():
-    buscador = input("Ingrese el nombre del producto que quiere modificar: ")
+def Modificar_Producto(buscador,añadir_cantidad,añadir_producto,añadir_precio):
     posicion = productos.index(buscador)
-    añadir_cantidad = int(input("Ingrese la cantidad de su producto: "))
-    añadir_producto = input("Ingrese el nombre de su producto: ")
-    añadir_precio = float(input("Ingrese el precio de su producto: "))
-
     cantidad[posicion] = añadir_cantidad
     productos[posicion] = añadir_producto
     precio[posicion] = añadir_precio
@@ -47,13 +37,13 @@ def Exportar_Excel():
         })
     df.to_excel("inventario.xlsx", index=False)
 
-def Borrar_Producto():
-    buscador = input("Ingrese el nombre del producto que quiere borrar: ")
+def Borrar_Producto(buscador):
+   
     if buscador in productos:
         posicion = productos.index(buscador)
         del cantidad[posicion]
         del productos[posicion]
         del precio[posicion]
-        print(f"El producto {buscador} ha sido borrado exitosamente.")
+        return (f"El producto {buscador} ha sido borrado exitosamente.")
     else:
-        print(f"El producto {buscador} no se encuentra en la lista.")
+        return (f"El producto {buscador} no se encuentra en la lista.")
